1f57cb5ab178e67a09578d407e74824ee4ce3c9e
Diogo Martins <nunodiogomartins@gmail.com>
2019-12-17 18:10:04 +0000

    Update App
    Save categories in local storage so we can persist data between uses

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import './styles.css';
 
@@ -9,14 +9,27 @@ import Chart from './components/Chart';
 import NavBar from './components/NavBar';
 
 function App() {
-	const [shouldShowAddCategory, setShouldShowAddCategory] = useState(true);
+	const [shouldShowAddCategory, setShouldShowAddCategory] = useState(false);
 	const [categories, setCategories] = useState([]);
 
 	const addCategory = category => {
 		const categoriesAux = [...categories, category];
 		setCategories(categoriesAux);
 		setShouldShowAddCategory(false);
+		localStorage.setItem('categories', JSON.stringify(categoriesAux));
 	}
+
+	useEffect(() => {
+		const categories = JSON.parse(localStorage.getItem('categories'));
+		if (categories) {
+			setCategories(categories);
+			setShouldShowAddCategory(false);
+		}
+
+		if (!categories) {
+			setShouldShowAddCategory(true);
+		}
+	}, []);
 
 	return (
 		<div className="App">
