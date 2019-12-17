665ee6c5d27328ad1e467cfad64de8c0c20ec09d
Diogo Martins <nunodiogomartins@gmail.com>
2019-12-17 16:16:41 +0000

    Create base app structure and style

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import './styles.css';
+
+import AddBill from './components/AddBill';
+import AddCategory from './components/AddCategory';
+import BillsTable from './components/BillsTable';
+import Chart from './components/Chart';
+import NavBar from './components/NavBar';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [shouldShowAddCategory, setShouldShowAddCategory] = useState(true);
+	return (
+		<div className="App">
+			{shouldShowAddCategory ? (
+				<AddCategory />
+			) : (
+					<div>
+						<NavBar />
+						<div className="container">
+							<div className="box">
+								<BillsTable />
+							</div>
+							<div className="box">
+								<Chart />
+							</div>
+						</div>
+					</div>
+				)
+			}
+		</div>
+	);
 }
 
 export default App;
